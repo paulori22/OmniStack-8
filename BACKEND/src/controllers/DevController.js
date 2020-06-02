4955910b97d680bbe8f95f6efd0dfbd56b406658
Paulo <paulinhoboneti@yahoo.com.br>
2020-06-02 08:37:53 -0300

    Códigos Finalizados da Semana Omnistack 8.0

---
+++ BACKEND/src/controllers/DevController.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+const Dev = require("../models/Dev");
+module.exports = {
+  async index(req, res) {
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        { _id: { $nin: loggedDev.likes } },
+        { _id: { $nin: loggedDev.dislikes } }
+      ]
+    });
+
+    return res.json(users);
+  },
+  async store(req, res) {
+    const { username } = req.body;
+
+    const userExists = await Dev.findOne({ user: username });
+
+    if (userExists) {
+      return res.json(userExists);
+    }
+
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+
+    let { name, bio, avatar_url: avatar } = response.data;
+
+    if (!name) name = "Sem nome no GIT";
+    const dev = await Dev.create({
+      name,
+      user: username,
+      bio,
+      avatar
+    });
+
+    return res.json(dev);
+  }
+};
---
+++ BACKEND/src/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const routes = require("./routes");
+
+const cors = require("cors");
+
+const server = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@clusterdev-zr24l.mongodb.net/omnistack?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+
+server.use(cors());
+
+server.use(express.json());
+
+server.use(routes);
+
+server.listen(3333);
